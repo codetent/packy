2c5ca2c624e41753cb7fdc26633549968e744c5f
codetent <22278777+codetent@users.noreply.github.com>
2019-07-03 02:30:20 +0200

    Add tox-travis

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
 
 setup(
     name='packy',  # Required
-    version='0.1.1',
+    version='0.1.2',
     description='Package manager for downloading packages ' \
                 'from content providers',
     long_description=long_description,
